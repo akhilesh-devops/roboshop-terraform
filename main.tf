326241653a39e45f6c456bc779231d0a7af10f47
AKhilesh <kothapallyakhil123@gmail.com>
2024-11-02 17:14:42 +0530

    test

--- main.tf
+++ main.tf
@@ -1,5 +1,10 @@
-variable "test" {}
+module "components" {
 
-output "test" {
-  value = var.test
+  for_each                = var.components
+
+  source                  = "git::https://github.com/akhilesh-devops/tf-module-basic.git"
+  zone_id                 = var.zone_id
+  vpc_security_group_ids  = var.vpc_security_group_ids
+  name                    = lookup(each.value, "name")
+  instance_type           = lookup(each.value, "instance_type")
 }
