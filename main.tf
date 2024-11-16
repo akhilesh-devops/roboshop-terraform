81ca09d002dadd143cd0c302d8ecd4f7b84a8371
AKhilesh <kothapallyakhil123@gmail.com>
2024-11-16 16:38:37 +0530

    test

--- main.tf
+++ main.tf
@@ -1,11 +1,13 @@
 module "vpc" {
   source = "git::https://github.com/akhilesh-devops/tf-module-vpc.git"
 
-  for_each  = var.vpc
-  cidr      = lookup(each.value, "cidr", null)
-  subnets   = lookup(each.value, "subnets", null)
-  default_vpc_id = var.default_vpc_id
-  default_vpc_cidr = var.default_vpc_cidr
+  for_each                   = var.vpc
+  cidr                       = lookup(each.value, "cidr", null)
+  subnets                    = lookup(each.value, "subnets", null)
+  default_vpc_id             = var.default_vpc_id
+  default_vpc_cidr           = var.default_vpc_cidr
   default_vpc_route_table_id = var.default_vpc_route_table_id
+  tags                       = var.tags
+  env                        = var.env
 }
 
